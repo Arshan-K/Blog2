98a220c8be89a1b910e3e0fb9e3a506d5d396c28
unknown <arshankhan9326863763@gmail.com>
2023-08-19 00:17:51 +0530

    changes that is for environment variable

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ const cors = require("cors");
 // });   
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: DB_HOST,
+  user: DB_USER,                                  //process.env.
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
   multipleStatements: true
 });
 
